d75ca31f71fe60585157d3ab12191ac84737d988
bulutluoz <abbulutluoz@gmail.com>
2023-11-22 11:49:16 +0100

    7.gun sonu

--- src/test/java/tests/day07_testBaseClass_Dropdown/C03_DropdownMenu.java
+++ src/test/java/tests/day07_testBaseClass_Dropdown/C03_DropdownMenu.java
@@ -1,11 +1,14 @@
 package tests.day07_testBaseClass_Dropdown;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import utilities.ReusableMethods;
 import utilities.TestBase;
+
+import java.util.List;
 
 public class C03_DropdownMenu extends TestBase {
 
@@ -23,11 +26,35 @@ public class C03_DropdownMenu extends TestBase {
 
         selectGun.selectByIndex(5);
 
-        //	2. Dogum tarihi ay seçeneğinden value kullanarak Nisan’i secin
-        //	3. Dogum tarihi yil seçeneğinden visible text kullanarak 1990’i secin
-        //	4. Ay dropdown menüdeki tum değerleri(value) yazdırın
-        //	5. Ay Dropdown menusunun boyutunun 30 olduğunu test edin
+        //	2. Dogum tarihi ay seçeneğinden visibleText kullanarak Nisan’i secin
 
-        ReusableMethods.bekle(5);
+        WebElement ayDdm = driver.findElement(By.xpath("(//select[@class='form-control'])[2]"));
+        Select selectAy = new Select(ayDdm);
+        selectAy.selectByVisibleText("Nisan");
+
+        //	3. Dogum tarihi yil seçeneğinden visible text kullanarak 1990’i secin
+        WebElement yilDdm = driver.findElement(By.xpath("(//select[@class='form-control'])[3]"));
+        Select selectYil = new Select(yilDdm);
+        selectYil.selectByVisibleText("1990");
+
+        //  4. Secilen degerleri konsolda yazdirin
+        //     Dropdown menude secim yaptiktan sonra
+        //     yapilan secimi yazdirmak isterseniz .getFirstSelectedOption().getText() kullaniriz
+
+        System.out.println("Secilen tarih : "+selectGun.getFirstSelectedOption().getText()+
+                           " " + selectAy.getFirstSelectedOption().getText()+
+                           " " + selectYil.getFirstSelectedOption().getText());
+
+        //	5. Ay dropdown menüdeki tum değerleri(value) yazdırın
+        List<WebElement> ayOpsiyonElementleriList = selectAy.getOptions();
+
+        System.out.println(ReusableMethods.stringListeDonustur(ayOpsiyonElementleriList));
+        // [Ay, Ocak, Şubat, Mart, Nisan, Mayıs, Haziran, Temmuz, Ağustos, Eylül, Ekim, Kasım, Aralık]
+
+        //	6. Ay Dropdown menusunun boyutunun 30 olduğunu test edin
+
+        Assert.assertEquals(30, ayOpsiyonElementleriList.size());
+
+        ReusableMethods.bekle(2);
     }
 }
---
+++ src/test/java/tests/day07_testBaseClass_Dropdown/C04_DropdownMenu.java
@@ -0,0 +1,59 @@
+package tests.day07_testBaseClass_Dropdown;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+public class C04_DropdownMenu extends TestBase {
+
+    @Test
+    public void dropdowntesti(){
+    //1. http://zero.webappsecurity.com/ Adresine gidin
+        driver.get("http://zero.webappsecurity.com/");
+    //2. Sign in butonuna basin
+        driver.findElement(By.id("signin_button")).click();
+    //3. Login kutusuna “username” yazin
+        WebElement loginKutusu = driver.findElement(By.id("user_login"));
+        loginKutusu.sendKeys("username");
+    //4. Password kutusuna “password” yazin
+        WebElement passwordKutusu = driver.findElement(By.id("user_password"));
+        passwordKutusu.sendKeys("password");
+    //5. Sign in tusuna basin, back tusuna basarak sayfaya donun
+        driver.findElement(By.xpath("//*[@name='submit']")).click();
+        driver.navigate().back();
+    //6. Online banking menusunden Pay Bills sayfasina gidin
+        driver.findElement(By.id("onlineBankingMenu")).click();
+        driver.findElement(By.id("pay_bills_link")).click();
+    //7. “Purchase Foreign Currency” tusuna basin
+        driver.findElement(By.linkText("Purchase Foreign Currency")).click();
+    //8. “Currency” dropdown menusunden Eurozone’u secin
+        WebElement currencyDdm = driver.findElement(By.id("pc_currency"));
+        Select selectCurrency = new Select(currencyDdm);
+        selectCurrency.selectByValue("EUR");
+    //9. “amount” kutusuna bir sayi girin
+        driver.findElement(By.id("pc_amount")).sendKeys("100");
+    //10. “US Dollars” in secilmedigini test edin
+        String unexpectedOptionYazisi = "US Dollars";
+        String actualOptionYazisi = selectCurrency.getFirstSelectedOption().getText();
+
+        Assert.assertNotEquals(unexpectedOptionYazisi,actualOptionYazisi);
+    //11. “Selected currency” butonunu secin
+        driver.findElement(By.id("pc_inDollars_false")).click();
+    //12. “Calculate Costs” butonuna basin sonra “purchase” butonuna basin
+        driver.findElement(By.id("pc_calculate_costs")).click();
+        driver.findElement(By.id("purchase_cash")).click();
+    //13. “Foreign currency cash was successfully purchased.” yazisinin ciktigini kontrol edin.
+        WebElement uyariYazisiElementi = driver.findElement(By.id("alert_content"));
+
+        String expectedUyariYazisi = "Foreign currency cash was successfully purchased.";
+        String actualUyariYazisi = uyariYazisiElementi.getText();
+
+        Assert.assertEquals(expectedUyariYazisi,actualUyariYazisi);
+
+        ReusableMethods.bekle(3);
+    }
+}
